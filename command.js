d174ea5188d898c7b442c6c78011afd1ecc7a6ee
Tim Hung <thung1@binghamton.edu>
2015-03-08 08:55:35 +0100

    Switch cases in command.js.

---
+++ command.js
@@ -0,0 +1,31 @@
+var parseCommand = function(String line){
+	var words = line.split(" ");
+
+    switch (words[0].toLowerCase()) {
+    case "help":
+        //do thing
+        break;
+    case "giphy":
+        //do thing
+        break;
+    case "artsy":
+        //do thing
+        break;
+    case "etsy":
+        //do thing
+        break;
+    case "blockchain":
+        //do thing
+        break;
+    case "orderx":
+        //do thing
+        break;
+    default:
+    	//do thing
+
+
+}
+
+// var stringsToArray = function(String line){
+// 	var words = line.split(" ");
+// }

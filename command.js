bde7b409d45af0c9d867bb5f25d2620ae38f2c7d
Tim Hung <thung1@binghamton.edu>
2015-03-08 09:51:31 +0100

    Spawned etsy.js.

--- command.js
+++ command.js
@@ -1,28 +1,32 @@
 var exports = module.exports = {};
 var giphy = require('./giphy.js');
+var etsy = require('./etsy.js');
+var file = require('read-file');
 
 exports.parseCommand = function(line){
 	var words = line.split(" ");
+    var query = words.slice(1, words.length);
 
     switch (words[0].toLowerCase()) {
         case "help":
-        //do thing
-        break;
+            help = file.readFileSync(__dirname + '/txt/help.txt');
+            cli.output("\n" + help + "\n");
+            break;
         case "giphy":
-        giphy.parseCommand(words.slice(1, words.length));        
-        break;
+            giphy.parseCommand(query);        
+            break;
         case "artsy":
-        //do thing
-        break;
+            //do thing
+            break;
         case "etsy":
-        //do thing
-        break;
+            etsy.parseCommand(query);
+            break;
         case "blockchain":
-        //do thing
-        break;
+            //do thing
+            break;
         case "orderx":
-        //do thing
-        break;
+            //do thing
+            break;
         default:
     	//do thing
     }

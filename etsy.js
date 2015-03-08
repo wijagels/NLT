5658e17318339d998ae7b7f7e3631d5e91b0709e
William Jagels <will.jagels@gmail.com>
2015-03-08 11:07:16 -0400

    Etsy

--- etsy.js
+++ etsy.js
@@ -1,12 +1,27 @@
 var exports = module.exports = {};
-var etsyjs = require('etsy-js');
-var client = etsyjs.client(keys.ETSY_KEY);
+var keys = require('./keys.js');
+var utils = require('./utils.js');
+var request = require('request');
+var prompt = require('prompt');
 
 exports.parseCommand = function(words) {
     var query = words.slice(1, words.length).toString().replace(',', ' ');
     switch (words[0].toLowerCase()) {
-        case "search":
-            console.log("etsy search: " + query);
+        case "trending":
+            request.get('https://openapi.etsy.com/v2/listings/trending?limit=25&api_key=' + keys.ETSY_KEY, {}, function(error, response, body) {
+                var results = JSON.parse(body).results;
+                for(var el in results) {
+                    console.log(el + " " + results[el]['title']);
+                }
+                prompt.start();
+                prompt.get(['selection'], function(err, result) {
+                    console.log(results[result.selection]);
+                    request.get('https://openapi.etsy.com/v2/listings/' + results[result.selection]['listing_id'] + '/images?api_key=' + keys.ETSY_KEY, {}, function(error,response,body) {
+                        console.log(JSON.parse(body).results);
+                        utils.getImage(JSON.parse(body).results[1]['url_570xN']);
+                    });
+                });
+            });
             break;
         case "category":
             console.log("etsy category : " + query);
@@ -22,5 +37,5 @@ exports.parseCommand = function(words) {
 //Functions:
 
 exports.random = function() {
-    
+
 }

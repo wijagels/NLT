3b7d5a03945a7e108d2efcd39a5b7b7614c4a16e
William Jagels <will.jagels@gmail.com>
2015-03-08 23:10:52 -0400

    Work from end of hackathon

--- utils.js
+++ utils.js
@@ -2,12 +2,14 @@ var exports = module.exports = {};
 
 var ImageToAscii = require('image-to-ascii');
 var Download = require('download');
+var gif = require('gif-explode')
+var fs = require('fs')
 
 exports.getImage = function(url) {
     var download = new Download({extract: true, strip: 1, mode: '755'})
-        .get(url)
-        .dest(__dirname + "/tmp")
-        .rename("tmp");
+    .get(url)
+    .dest(__dirname + "/tmp")
+    .rename("tmp");
     download.run(function(err, files) {
         if(err)
             throw err;
@@ -20,4 +22,26 @@ exports.getImage = function(url) {
 exports.toAscii = function(cmd) {
     var url = cmd.toString().split(' ');
     exports.getImage(cmd.toString());
+}
+
+exports.gif = function(url) {
+
+  var download = new Download({extract: true, strip: 1, mode: '755'})
+  .get(url)
+  .dest(__dirname + "/tmp")
+  .rename("tmp");
+  download.run(function(err, files) {
+      if(err)
+          throw err;
+      fs.createReadStream(__dirname + '/tmp/tmp')
+        .pipe(gif(function(frame) {
+          frame.pipe(fs.createWriteStream(
+            'frame.gif'
+          ))
+          ImageToAscii(__dirname + "/" + "frame.gif" , function(err, converted, i) {
+              console.log(err || converted);
+          });
+        }))
+      
+  });
 }
